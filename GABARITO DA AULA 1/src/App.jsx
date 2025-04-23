a71fe36303c68d9ce05744f6549fbe7e564d7fc5
Weverton Fernandes <budpoki@gmail.com>
2025-04-22 22:12:30 -0300

    Add arquivos
    arquivos de códigos e dependências adicionados

---
+++ GABARITO DA AULA 1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/header'
+import Main from './components/Main'
+
+const App = () => {
+  return(
+  <>
+    <Header />
+   <Main />
+  </>
+
+  )
+};
+export default App
---
+++ GABARITO DA AULA 1/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return <div className="header">
+    <img src="src/assets/logo/spotify-logo.png" alt="" />
+    <a className='header__link' href="/">
+        <h1>spotify</h1> 
+    </a>
+  </div>;
+
+
+};
+
+export default Header;
